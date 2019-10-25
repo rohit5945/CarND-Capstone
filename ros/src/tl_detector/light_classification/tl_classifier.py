16e57ba0a29dadda89c17e1d895c0975b57aed7e
rohit5945 <er.rohit1502@gmail.com>
2019-10-25 10:07:29 +0530

    code modified for TL classifer and waypoint updater

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,33 @@
 from styx_msgs.msg import TrafficLight
+import cv2
+from keras.models import load_model
+from numpy import newaxis
+import numpy as np
+import tensorflow as tf 
+import os
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
-
+        path = os.getcwd()
+        self.model = load_model(path+'/light_classification/real_tl_classifier_new.h5') 
+    
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
-
         Args:
             image (cv::Mat): image containing the traffic light
-
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
         """
-        #TODO implement light color prediction
+        image = cv2.resize(image, (800, 600)) 
+        image = image.astype(float)
+        image = image / 255.0
+        scores = self.model.predict(image)
+        #print('Classification:' ,classification[0])
+
+        if(scores[0] == 1): # 'Traffic Light: Red', 'Traffic Light: Green/Yellow', 'No Traffic Light' or [0,1,2]
+            
+            print("Classifier predicted RED light")
+            return TrafficLight.RED
+        
+        print("Classifier predicted NOT red light")
         return TrafficLight.UNKNOWN
